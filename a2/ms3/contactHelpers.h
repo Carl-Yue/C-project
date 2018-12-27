1b8ab5f828787441c7b2978764fd3e79e260aab1
Carl-Yue <45949578+Carl-Yue@users.noreply.github.com>
2018-12-27 13:49:16 -0500

    Add files via upload

---
+++ a2/ms3/contactHelpers.h
@@ -0,0 +1,18 @@
+void clearKeyboard (void);
+void pause (void);
+int getInt (void);
+int getIntInRange (int, int);
+int yes (void);
+int menu (void);
+void ContactManagerSystem (void);
+void getTenDigitPhone (char[]);
+int findContactIndex(const struct Contact[], int, const char[]);
+void displayContactHeader(void);
+void displayContactFooter(int);
+void displayContact;
+void displayContacts(const struct Contact[], int);
+void searchContacts(const struct Contact[], int);
+void addContact(struct Contact[], int);
+void updateContact(struct Contact[], int);
+void deleteContact(struct Contact[], int);
+void sortContacts(struct Contact[], int);
