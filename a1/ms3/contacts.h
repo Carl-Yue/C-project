4fd7878c777c084c1acaafc5352c7093c97036ff
Carl-Yue <45949578+Carl-Yue@users.noreply.github.com>
2018-12-27 12:33:47 -0500

    Add files via upload

--- a1/ms3/contacts.h
+++ a1/ms3/contacts.h
@@ -1,1 +1,33 @@
-
+/* -------------------------------------------
+Name:
+Student number:
+Email:
+Section:
+Date:
+----------------------------------------------
+Assignment: 1
+Milestone: 1
+----------------------------------------------*/
+//Structure type Name declaration
+struct Name {
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
+};
+struct Address {
+	int streetNumber;
+	char street[41];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[41];
+};
+struct Numbers {
+	char cell[21];
+	char home[21];
+	char business[21];
+};
+struct Contact {
+	char name;
+	char address;
+	char numbers;
+};
